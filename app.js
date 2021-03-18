ba1218009ccf4dfe3b5f1035bc5662774ca76ca3
chase-abbott <cabbott93@gmail.com>
2021-03-18 13:48:05 -0700

    finsished the js

--- app.js
+++ app.js
@@ -1,5 +1,49 @@
 // import functions and grab DOM elements
+const addFirstInput = document.getElementById("first-addition");
+const addSecondInput = document.getElementById("second-addition");
+const addButton = document.getElementById("addition-button");
+const addAnswer = document.getElementById("addition-answer");
 
-// initialize state
 
-// set event listeners to update state and DOM
+addButton.addEventListener('click', () => {
+    const value1 = addFirstInput.valueAsNumber;
+    const value2 = addSecondInput.valueAsNumber;
+    const sum = value1 + value2;
+    addAnswer.textContent = sum;
+})
+
+const subFirstInput = document.getElementById("first-sub");
+const subSecondInput = document.getElementById("second-sub");
+const subButton = document.getElementById("sub-button");
+const subAnswer = document.getElementById("sub-answer");
+
+subButton.addEventListener('click', () => {
+    const value1 = subFirstInput.valueAsNumber;
+    const value2 = subSecondInput.valueAsNumber;
+    const sub = value1 - value2;
+    subAnswer.textContent = sub;
+})
+
+const multFirstInput = document.getElementById("first-mult");
+const multSecondInput = document.getElementById("second-mult");
+const multButton = document.getElementById("mult-button");
+const multAnswer = document.getElementById("mult-answer");
+
+multButton.addEventListener('click', () => {
+    const value1 = multFirstInput.valueAsNumber;
+    const value2 = multSecondInput.valueAsNumber;
+    const mult = value1 * value2;
+    multAnswer.textContent = mult;
+})
+
+const divideFirstInput = document.getElementById("first-divide");
+const divideSecondInput = document.getElementById("second-divide");
+const divideButton = document.getElementById("divide-button");
+const divideAnswer = document.getElementById("divide-answer");
+
+divideButton.addEventListener('click', () => {
+    const value1 = divideFirstInput.valueAsNumber;
+    const value2 = divideSecondInput.valueAsNumber;
+    const divide = value1 / value2;
+    divideAnswer.textContent = divide;
+})
