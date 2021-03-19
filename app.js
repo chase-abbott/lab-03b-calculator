e4b2967eadf9fd493e92e92a4e4fbc07fd1d6294
chase-abbott <cabbott93@gmail.com>
2021-03-19 11:26:07 -0700

    fixing it

--- app.js
+++ app.js
@@ -1,56 +1,19 @@
 
 import { handleAdd, handleSub, handleMult, handleDiv} from './handlers.js'
 
-const addFirstInput = document.getElementById("first-addition");
-const addSecondInput = document.getElementById("second-addition");
 const addButton = document.getElementById("addition-button");
-const addAnswer = document.getElementById("addition-answer");
+addButton.addEventListener('click', handleAdd) 
 
-addButton.addEventListener('click', () => {
-    const value1 = addFirstInput.valueAsNumber;
-    const value2 = addSecondInput.valueAsNumber;
-    const sum = handleAdd;
 
-    addAnswer.textContent = sum;
-    // can set sum = handleAdd once figured out
-})
-
-const subFirstInput = document.getElementById("first-sub");
-const subSecondInput = document.getElementById("second-sub");
 const subButton = document.getElementById("sub-button");
-const subAnswer = document.getElementById("sub-answer");
+subButton.addEventListener('click', handleSub)
 
-subButton.addEventListener('click', () => {
-    const value1 = subFirstInput.valueAsNumber;
-    const value2 = subSecondInput.valueAsNumber;
-    const sub = value1 - value2;
-
-    subAnswer.textContent = sub;
-})
-
-const multFirstInput = document.getElementById("first-mult");
-const multSecondInput = document.getElementById("second-mult");
 const multButton = document.getElementById("mult-button");
-const multAnswer = document.getElementById("mult-answer");
+multButton.addEventListener('click', handleMult)
 
-multButton.addEventListener('click', () => {
-    const value1 = multFirstInput.valueAsNumber;
-    const value2 = multSecondInput.valueAsNumber;
-    const mult = value1 * value2;
-
-    multAnswer.textContent = mult;
-})
-
-const divideFirstInput = document.getElementById("first-divide");
-const divideSecondInput = document.getElementById("second-divide");
 const divideButton = document.getElementById("divide-button");
-const divideAnswer = document.getElementById("divide-answer");
+divideButton.addEventListener('click', handleDiv)
 
-divideButton.addEventListener('click', () => {
-    const value1 = divideFirstInput.valueAsNumber;
-    const value2 = divideSecondInput.valueAsNumber;
-    const divide = value1 / value2;
-    
-    divideAnswer.textContent = divide;
-})
+
+
 
--- handlers.js
+++ handlers.js
@@ -1,7 +1,42 @@
 
 import { addTwoNumbers, subTwoNumbers, multTwoNumbers, divideTwoNumbers } from './mathUtils.js'
 
-export const handleAdd = addTwoNumbers();
-export const handleSub = subTwoNumbers();
-export const handleMult = multTwoNumbers();
-export const handleDiv = divideTwoNumbers();
+const addFirstInput = document.getElementById("first-addition");
+const addSecondInput = document.getElementById("second-addition");
+const addAnswer = document.getElementById("addition-answer");
+
+export function handleAdd () {
+    const value1 = addFirstInput.valueAsNumber;
+    const value2 = addSecondInput.valueAsNumber;
+    addAnswer.textContent = addTwoNumbers(value1, value2);
+} 
+
+const subFirstInput = document.getElementById("first-sub");
+const subSecondInput = document.getElementById("second-sub");
+const subAnswer = document.getElementById("sub-answer");
+
+export function handleSub () {
+    const value1 = subFirstInput.valueAsNumber;
+    const value2 = subSecondInput.valueAsNumber;
+    subAnswer.textContent = subTwoNumbers(value1, value2);
+}
+
+const multFirstInput = document.getElementById("first-mult");
+const multSecondInput = document.getElementById("second-mult");
+const multAnswer = document.getElementById("mult-answer");
+
+export function handleMult () {
+    const value1 = multFirstInput.valueAsNumber;
+    const value2 = multSecondInput.valueAsNumber;
+    multAnswer.textContent = multTwoNumbers(value1, value2);
+}
+
+const divideFirstInput = document.getElementById("first-divide");
+const divideSecondInput = document.getElementById("second-divide");
+const divideAnswer = document.getElementById("divide-answer");
+
+export function handleDiv() {
+    const value1 = divideFirstInput.valueAsNumber;
+    const value2 = divideSecondInput.valueAsNumber;
+    divideAnswer.textContent = divideTwoNumbers(value1, value2);  
+} 
