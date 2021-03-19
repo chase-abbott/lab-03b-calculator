ab013315c3eca44e2160f0e72e959643bf4d8b88
chase-abbott <cabbott93@gmail.com>
2021-03-19 11:00:07 -0700

    fixing stuff

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ addButton.addEventListener('click', () => {
     const value1 = addFirstInput.valueAsNumber;
     const value2 = addSecondInput.valueAsNumber;
     const sum = handleAdd;
+
     addAnswer.textContent = sum;
     // can set sum = handleAdd once figured out
 })
@@ -23,6 +24,7 @@ subButton.addEventListener('click', () => {
     const value1 = subFirstInput.valueAsNumber;
     const value2 = subSecondInput.valueAsNumber;
     const sub = value1 - value2;
+
     subAnswer.textContent = sub;
 })
 
@@ -35,6 +37,7 @@ multButton.addEventListener('click', () => {
     const value1 = multFirstInput.valueAsNumber;
     const value2 = multSecondInput.valueAsNumber;
     const mult = value1 * value2;
+
     multAnswer.textContent = mult;
 })
 
@@ -47,6 +50,7 @@ divideButton.addEventListener('click', () => {
     const value1 = divideFirstInput.valueAsNumber;
     const value2 = divideSecondInput.valueAsNumber;
     const divide = value1 / value2;
+    
     divideAnswer.textContent = divide;
 })
 
