fc11dd41281e294c387d3c26d7e1241456e0fd17
chase-abbott <cabbott93@gmail.com>
2021-03-18 17:59:05 -0700

    add functions

---
+++ mathUtils.js
@@ -0,0 +1,15 @@
+export function add(number, otherNumber) {
+    return number + otherNumber;
+}
+
+export function sub(number, otherNumber) {
+    return number + otherNumber;
+}
+
+export function mult(number, otherNumber) {
+    return number + otherNumber;
+}
+
+export function divide(number, otherNumber) {
+    return number + otherNumber;
+}
