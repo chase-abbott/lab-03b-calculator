a96a735cb4657835640693ba4bad03efab50a606
chase-abbott <cabbott93@gmail.com>
2021-03-19 18:01:18 -0700

    formatting

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
-
 import { handleAdd, 
          handleSub, 
          handleMult, 
-         handleDiv} from './handlers.js'
+         handleDiv } from './handlers.js';
 
-const addition = document.getElementById("addition");
-const equalsButton = document.getElementById("equals");
-const subtraction = document.getElementById("subtraction");
-const multiplication = document.getElementById("multiplication");
-const division = document.getElementById("division");
+const addition = document.getElementById('addition');
+const equalsButton = document.getElementById('equals');
+const subtraction = document.getElementById('subtraction');
+const multiplication = document.getElementById('multiplication');
+const division = document.getElementById('division');
 
-function doMath () {
-    if (addition) {
-        handleAdd();
-    } else if (subtraction) {
-        handleSub();
-    } else if (multiplication) {
-        handleMult();
-    } else if (division) {
-        handleDiv();
-    }
+// function doMath () {
+//     if (addition) {
+//         handleAdd;
+//     } else if (subtraction) {
+//         handleSub;
+//     } else if (multiplication) {
+//         handleMult;
+//     } else if (division) {
+//         handleDiv;
+//     }
+// };
+
+
+if (addition) {
+    equalsButton.addEventListener('click', handleAdd);
+ } else if (subtraction) {
+    equalsButton.addEventListener('click', handleSub);
+} else if (multiplication) {
+    equalsButton.addEventListener('click', handleMult);
+} else if (division) {
+    equalsButton.addEventListener('click', handleDiv); 
 };
-
-equalsButton.addEventListener('click', doMath);
-
-
                                                                                                              
 
 
